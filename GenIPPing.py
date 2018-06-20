73cbd2653cc0393aa4226c6ac69de6038981b5b3
jamesobradley <40287304+jamesobradley@users.noreply.github.com>
2018-06-20 06:34:12 -0700

    Testing Upload to GitHub
    Testing process of uploading files to GitHub

---
+++ GenIPPing.py
@@ -0,0 +1,23 @@
+#This program will generate the appropriate number of 4th octect values based on the entered subnet bits.
+
+subnt = 0
+sub_calc=0
+
+subnt = input("Enter the subnet #:  ")
+28
+subnt=int(subnt)
+
+if (subnt>23<33)==True:
+   sub_calc= int(2**(32-subnt))
+   a=0
+   while a < sub_calc:
+      a=str(a)
+      print("10.99.1."+a)
+      print(sub_calc)
+      a=int(a)
+      a += 1
+else:
+   print("Try a subnet length that is /24 or greater.")
+
+
+
